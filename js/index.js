b68403067a3e161d62d4598104ed501540558cfe
lowit <ea@lowit.ru>
2023-01-10 15:35:56 +0300

    master: v2

--- js/index.js
+++ js/index.js
@@ -1,59 +1,75 @@
-$(document).ready(function () {
-
+$("body").ready(function () {
   // random backgrounds
   var bgm = [
-    'images/1.jpg',
-    'images/2.jpg',
-    'images/3.jpg',
-    'images/4.jpg',
-    'images/5.jpg',
-    'images/6.jpg',
-    'images/7.jpg',
-    'images/8.jpg',
-    'images/9.jpg',
-    'images/10.jpg',
-    'images/11.jpg',
-    'images/12.jpg',
-    'images/13.jpg',
-    'images/14.jpg',
-    'images/15.jpg',
-    'images/16.jpg',
-    'images/17.jpg',
-    'images/18.jpg',
-    'images/19.jpg',
+    "images/1.jpg",
+    "images/2.jpg",
+    "images/3.jpg",
+    "images/4.jpg",
+    "images/5.jpg",
+    "images/6.jpg",
+    "images/7.jpg",
+    "images/8.jpg",
+    "images/9.jpg",
+    "images/10.jpg",
+    "images/11.jpg",
+    "images/12.jpg",
+    "images/13.jpg",
+    "images/14.jpg",
+    "images/15.jpg",
+    "images/16.jpg",
+    "images/17.jpg",
+    "images/18.jpg",
+    "images/19.jpg",
   ];
 
-  $('body').css({
-    'background': 'url(' + bgm[Math.floor(Math.random() * bgm.length)] + ') no-repeat center center fixed',
-    'background-color': '#000',
-    'background-size': 'cover',
+  $("body").css({
+    background:
+      "url(" +
+      bgm[Math.floor(Math.random() * bgm.length)] +
+      ") no-repeat center center fixed",
+    "background-color": "#000",
+    "background-size": "cover",
   });
+});
 
-  function second_passed() {
-    $('.clock').removeClass('is-off');
+$(document).ready(function () {
+  Number.prototype.pad = function (n) {
+    for (var r = this.toString(); r.length < n; r = 0 + r);
+    return r;
+  };
+
+  function updateClock() {
+    var now = new Date();
+    var sec = now.getSeconds(),
+      min = now.getMinutes(),
+      hou = now.getHours(),
+      mo = now.getMonth(),
+      dy = now.getDate(),
+      yr = now.getFullYear();
+    var months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    var tags = ["mon", "d", "y", "h", "m", "s"],
+      corr = [months[mo], dy, yr, hou.pad(2), min.pad(2), sec.pad(2)];
+    for (var i = 0; i < tags.length; i++)
+      document.getElementById(tags[i]).firstChild.nodeValue = corr[i];
   }
-  setTimeout(second_passed, 2000)
 
-  $('.switcher').on('click', function (e) {
-    e.preventDefault();
-    $('.screen').toggleClass('glitch');
-  });
-
-
-  var newDate = new Date();
-  newDate.setDate(newDate.getDate());
+  // first time run
+  updateClock();
 
   setInterval(function () {
-
-    var hours = new Date().getHours();
-    var seconds = new Date().getSeconds();
-    var minutes = new Date().getMinutes();
-
-    var realTime = (hours < 10 ? '0' : '') + hours + ' : ' + (minutes < 10 ? '0' : '') + minutes + ' : ' + (seconds < 10 ? '0' : '') + seconds
-
-    $('.time').html(realTime);
-    $('.time').attr('data-time', realTime);
-
-  }, 1);
-
+    updateClock();
+  }, 1000);
 });
